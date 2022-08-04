6d1fd6983407fa1709b9a6bc548990888130652b
Vinícius Soares <vinivsp2304@hotmail.com>
2022-08-04 14:57:40 -0300

    Update index.js
    add: estrutura condicional no params para exibição de algumas informações

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.use(json());
 
 app.get("/", (req, res) => res.send("funcionou"));
 
+app.get("/teste/:nome?/:linguagem?", (req, res) => {
+    let nome = req.params.nome
+    let linguagem = req.params.linguagem
+
+    if(nome && linguagem){
+        res.send('Dev: ' + nome + ' | Linguagem: ' + linguagem)
+    } else if(nome && linguagem == null){
+        res.send('Dev: ' + nome + ' | Linguagem: Não há dados disponíveis')
+    }else {
+        res.send('Nada há dados disponíveis')
+    }
+
+});
+
 app.post("/", (req, res) => {
   const { a, b } = req.body;
 
