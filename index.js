c9f6bd44ef46863872203ef9eae9c94a5403f215
Vinícius Soares <vinivsp2304@hotmail.com>
2022-08-10 12:48:18 -0300

    add: ORM 'Pergunta'

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { json } = require("express");
 const express = require("express");
 const app = express();
 const connection = require("./database/database");
+// Models
+const perguntaModel = require("./model/Pergunta");
 
 // ### Database ###
 connection.authenticate().then(() => {
-  console.log("Conexão com o banco dados realizada.");
+  console.log("Conexão com o Banco de Dados realizada.");
 });
 
 app.listen(8080, () => console.log("Server working"));
@@ -14,4 +16,3 @@ app.use(json());
 app.get("/", (req, res) => res.send("backend rodando"));
 
 app.post("/perguntar", (req, res) => res.send("Pergunta enviada."));
-("");
---
+++ model/Pergunta.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const connection = require('../database/database')
+
+const Pergunta = connection.define("pergunta", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  titulo: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
+  descricao: {
+    type: Sequelize.STRING(500),
+    allowNull: false,
+  },
+});
+
+Pergunta.sync({force: false}).then(()=> {
+    console.log('Tabela Pergunta criada.')
+})
+// cria tabela caso não exista | 'force' se existe, não força a criação da tabela.
