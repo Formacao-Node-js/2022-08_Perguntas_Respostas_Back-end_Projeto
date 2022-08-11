52b4d69302a7ba8c8a536b588e5903811119185d
Vinícius Soares <vinivsp2304@hotmail.com>
2022-08-11 16:07:47 -0300

    aprendendo novo truque

---
+++ ignora.js
@@ -0,0 +1,23 @@
+/* ### PRATICANDO 'FOR... OF' ###*/
+
+/*
+
+
+console.log('a')
+
+const array = ['a', 'b', 'c', 'd']
+
+for (const valor in array) {
+    console.log(array[valor])
+}
+
+const obj = {
+    a: 'oi',
+    b: 'tchau',
+    c: 'beleza'
+}
+
+for (const valor of Object.entries(obj)) {
+    console.log(valor)}
+
+*/
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log('a')
+
+const array = ['a', 'b', 'c', 'd']
+
+for (const valor in array) {
+    console.log(array[valor])
+}
