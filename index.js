50f184bb96926e64d0ea0159aadb54d15f68da02
Vinícius Soares <vinivsp2304@hotmail.com>
2022-08-11 15:30:57 -0300

    update 'app.post' || add: alguns tratamentos de erro

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { json } = require("express");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const connection = require("./database/database");
 // Models
@@ -13,18 +14,20 @@ connection.authenticate().then(() => {
 app.listen(8711, () => console.log("Server working"));
 
 app.use(json());
+app.use(cors());
 
 app.get("/", (req, res) => res.send("backend rodando"));
 
 app.post("/perguntar", (req, res) => res.send("Pergunta enviada."));
 
-app.post("/salvarpergunta", (req, res) => {
+app.post("/salvarpergunta", async (req, res) => {
   const { titulo, descricao } = req.body;
-  Pergunta.create({
+  if (!descricao && !titulo) return res.send("Os campos: 'Título' e 'Descrição' não podem estar vazios");
+  if (!titulo) return res.send("O campo: 'Título' não pode estar vazio");
+  if (!descricao) return res.send("O campo: 'Descrição' não pode estar vazio");
+  await Pergunta.create({
     titulo: titulo,
     descricao: descricao,
-  }).then(() => {
-    console.log("Pergunta salva com sucesso.");
-    res.send("Pergunta salva com sucesso.");
   });
+  res.send("Pergunta salva com sucesso.");
 });
